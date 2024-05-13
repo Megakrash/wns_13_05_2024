393a6c73ec0c7c78d0f77e9da5897f2887525ffa
Mega Krash <jscattolini@gmail.com>
2024-05-13 12:15:26 +0200

    :sparkles: see list of country when create one

--- frontend/src/components/country/CountryForm.tsx
+++ frontend/src/components/country/CountryForm.tsx
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { postNewCountry } from "@/graphql/client";
-import { NewCountryInput } from "../../types";
+import { useQuery, useMutation } from "@apollo/client";
+import { postNewCountry, queryAllCountries } from "@/graphql/client";
+import { NewCountryInput, Country } from "../../types";
+import CountryCard from "../country/CountryCard";
 import { Button, TextField, Box } from "@mui/material";
 
-const CountryForm = (): JSX.Element => {
+const CreateCountryForm = (): JSX.Element => {
   const [formData, setFormData] = useState<NewCountryInput>({
     name: "",
     emoji: "",
     code: "",
   });
 
-  const [addCountry, { data, loading, error }] = useMutation(postNewCountry, {
+  const {
+    data: dataCountries,
+    loading: loadingCountries,
+    refetch,
+  } = useQuery<{ items: Country[] }>(queryAllCountries);
+  const countries: Country[] = dataCountries ? dataCountries.items : [];
+
+  const [addCountry, { loading, error }] = useMutation(postNewCountry, {
     variables: { data: formData },
     onCompleted: () => {
       alert("Country added successfully!");
-      setFormData({ name: "", emoji: "", code: "" }); // Reset the form
+      refetch();
     },
     onError: (error) => {
       alert("Error adding country: " + error.message);
+    },
+    update: (cache, { data: { addCountry } }) => {
+      const existingCountries = cache.readQuery<{ items: Country[] }>({
+        query: queryAllCountries,
+      });
+      if (existingCountries && addCountry) {
+        cache.writeQuery({
+          query: queryAllCountries,
+          data: { items: [...existingCountries.items, addCountry] },
+        });
+      }
     },
   });
 
@@ -33,68 +52,76 @@ const CountryForm = (): JSX.Element => {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      noValidate
-      sx={{
-        margin: "auto",
-        mt: 5,
-        width: { xs: "80%", sm: "60%" },
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "20px",
-      }}
-    >
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="name"
-        label="Nom du pays"
-        name="name"
-        autoComplete="name"
-        autoFocus
-        value={formData.name}
-        onChange={handleChange}
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="emoji"
-        label="Emoji du pays"
-        name="emoji"
-        autoComplete="emoji"
-        value={formData.emoji}
-        onChange={handleChange}
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="code"
-        label="Code du pays"
-        name="code"
-        autoComplete="code"
-        value={formData.code}
-        onChange={handleChange}
-      />
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="primary"
-        sx={{ mt: 3, mb: 2 }}
-        disabled={loading}
+    <div>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{
+          mt: 1,
+          width: {
+            xs: "80%",
+            sm: "60%",
+          },
+        }}
       >
-        Ajouter un pays
-      </Button>
-      {error && <p>Error submitting form: {error.message}</p>}
-    </Box>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="Nom du pays"
+          name="name"
+          autoComplete="name"
+          autoFocus
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="emoji"
+          label="Emoji du pays"
+          name="emoji"
+          autoComplete="emoji"
+          value={formData.emoji}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="code"
+          label="Code du pays"
+          name="code"
+          autoComplete="code"
+          value={formData.code}
+          onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+          disabled={loading}
+        >
+          Ajouter un pays
+        </Button>
+        {error && <p>Error submitting form: {error.message}</p>}
+      </Box>
+      <Box sx={{ display: "flex", flexWrap: "wrap", gap: "15px", mt: 3 }}>
+        {loadingCountries ? (
+          <p>Loading...</p>
+        ) : (
+          countries.map((country) => (
+            <CountryCard key={country.id} country={country} />
+          ))
+        )}
+      </Box>
+    </div>
   );
 };
 
-export default CountryForm;
+export default CreateCountryForm;
